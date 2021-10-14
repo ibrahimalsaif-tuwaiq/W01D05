dc0ec33c2695e4e833d5acd3bded2c12b6ffcf94
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-10-14 11:42:05 +0300

    JS Tasks

---
+++ dataTypes.js
@@ -0,0 +1,49 @@
+// Integer
+
+let myInt = 10;
+
+// String
+
+let myString = "Text";
+
+// Array
+
+let myArray = [5, 5, 10, 45];
+
+// Object
+
+const myObject = {
+  firstName: "ibrahim",
+  lastName: "alsaif",
+  score: 10,
+};
+
+// Display
+
+console.log(myInt);
+console.log(myString);
+console.log(myArray);
+console.log(myObject);
+
+// typeof
+
+console.log(typeof myInt);
+console.log(typeof myString);
+console.log(typeof myArray);
+console.log(typeof myObject);
+
+// Array with numbers and strings
+
+let typeArray = [5, 10, "hello", "world"]; //type object
+
+// typeof Array with numbers and strings
+
+console.log(typeof typeArray);
+
+// null variable and an undefined variable
+
+let varNull = null;
+let varundefined = undefined;
+
+console.log(varNull);
+console.log(varundefined);
---
+++ myVariables.js
@@ -0,0 +1,29 @@
+// 1
+
+let language = 'JavaScript';
+let score = 10;
+
+console.log(language);
+console.log(score);
+
+// 2
+
+let length = 50;
+let width = 20;
+
+console.log('The area of the rectangle is ' + length * width);
+
+// 3
+
+let num1 = 5;
+let num2 = 2;
+
+console.log('The reminder is ' + num1 % num2);
+
+// 4
+
+let div = Math.trunc(num1 / num2);
+
+let rem = num1 % num2;
+
+console.log(`${num1} / ${num2} = ${div} reminder ${rem}`);
